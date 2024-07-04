8a75da2a3872698c2fafd264da22db76e8f2a3b4
Al_27 <ahmedlaghfour@gmail.com>
2024-07-05 00:58:02 +0100

    annotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,9 +6,8 @@ import logging
 import re
 
 
-def filter_datum(fields: list, redaction: str, message: list, separator: str) -> list:
-    new_mess = message.copy()
-    for i in range(len(new_mess)):
-        for f in fields:
-            new_mess[i] = re.sub(f"(?<={f}=)[^{separator}]+",redaction, new_mess[i])
+def filter_datum(fields: list, redaction: str, message: list, separator: str) -> str:
+    new_mess = "\n".join(map(str,message))
+    for f in fields:
+        new_mess = re.sub(f"(?<={f}=)[^{separator}]+",redaction, new_mess)
     return new_mess
