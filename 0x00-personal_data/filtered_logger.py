3f06ee03e60e362e7cc5fa6f2dc3ba45e7239c7d
Al_27 <ahmedlaghfour@gmail.com>
2024-07-05 01:55:34 +0100

    annot

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -22,7 +22,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: typing.List[str]):
         self.fields = list(fields)
         super(RedactingFormatter, self).__init__(self.FORMAT)
 
