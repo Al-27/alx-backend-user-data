020cf3fd3ba428bc5c8be1a1e498c51f9b70a8cc
Al_27 <ahmedlaghfour@gmail.com>
2024-07-05 01:02:50 +0100

    update

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,12 +2,13 @@
 """
 Mod Doc
 """
+import typing
 import logging
 import re
 
 
-def filter_datum(fields: list, redaction: str, message: list, separator: str) -> str:
-    new_mess = "\n".join(map(str,message))
+def filter_datum(fields: typing.List[str], redaction: str, message: str, separator: str) -> str:
+    new_mess = message.copy()
     for f in fields:
         new_mess = re.sub(f"(?<={f}=)[^{separator}]+",redaction, new_mess)
     return new_mess
