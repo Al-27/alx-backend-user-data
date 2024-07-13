69343864f7e587d3f0ff991adaedf1a36825ad9b
Al-27 <125827852+Al-27@users.noreply.github.com>
2024-07-13 18:48:19 +0000

    auth_type condition typo

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -51,7 +51,7 @@ if __name__ == "__main__":
     port = getenv("API_PORT", "5000")
     auth_type = getenv("AUTH_TYPE")
     
-    if auth_type == "auth_basic" :
+    if auth_type == "basic_auth" :
         from api.v1.auth.basic_auth import BasicAuth
         auth = BasicAuth()
     elif auth_type == "auth":
