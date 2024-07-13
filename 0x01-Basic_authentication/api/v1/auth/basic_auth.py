34cc67d5d35f0dcf1a4cfe6e49d59d1cfc387f37
Al-27 <125827852+Al-27@users.noreply.github.com>
2024-07-13 19:34:10 +0000

    update b_a

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -26,6 +26,21 @@ class BasicAuth(Auth):
     def decode_base64_authorization_header(self, base64_authorization_header: str) -> str: 
         """function
         """
+        import codecs
+        try:
+            if base64_authorization_header:
+                if isinstance(base64_authorization_header, str):
+                    return codecs.decode(base64_authorization_header)
+        except Exception as e:
+            pass
 
         return None
 
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        """function
+        """
+        if decoded_base64_authorization_header:
+            if isinstance(decoded_base64_authorization_header, str):
+                if len(decoded_base64_authorization_header.split(":")) == 2:
+                    return tuple(decoded_base64_authorization_header.split(":"))
+        return (None,None)
