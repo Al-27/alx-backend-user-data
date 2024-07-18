ed3e68c1071ba00e03cf4617a599abf361a06012
Al-27 <125827852+Al-27@users.noreply.github.com>
2024-07-18 23:46:01 +0000

    more

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """APP
 """
-from flask import Flask, jsonify, request, session, abort
+from flask import Flask, jsonify, request, session, abort, redirect, url_for
 from auth import Auth
 import os
 
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.secret_key = str(os.urandom(24))
 
 @app.route("/")
-def root():
+def index():
     """ROUTE
     """
     return jsonify({"message": "Bienvenue"})
@@ -40,7 +40,57 @@ def login():
         session['session_id'] = AUTH.create_session(email)
         return jsonify({"email": email, "message": "logged in"})
     
-    return abort(401) 
+    abort(401) 
+
+@app.route("/sessions", methods=['DELETE'])
+def logout():
+    """logout
+    """
+    if "session_id" in request.cookies:
+        sess_id = request.cookies["session_id"]
+        user = AUTH.get_user_from_session_id(sess_id)
+        if user :
+            session.pop("session_id")
+            AUTH.destroy_session(user.id)
+            return redirect(url_for("index"))
+    abort(403)
+    
+@app.route('/profile')
+def profile():
+    """func
+    """
+    ss_id = request.cookies['session_id']
+    user = AUTH.get_user_from_session_id(ss_id)
+    if user:
+        return jsonify({"email": user.email})
+    
+    abort(403)
+
+@app.route('/reset_password', methods=["POST"])
+def get_reset_password_token():
+    """func
+    """
+    email = request.form['email']
+    try:
+        res_tok = AUTH.get_reset_password_token(email)
+    except:
+        abort(403)
+    
+    return jsonify({"email": email, "reset_token": res_tok})
+
+@app.route('/reset_password', methods=["PUT"])
+def update_password ():
+    """func
+    """
+    email = request.form['email']
+    res_tok = request.form['reset_token']
+    try:
+        AUTH.update_password(res_tok, request.form['new_password'])
+    except:
+        abort(403)
+    
+    return jsonify({"email": email, "reset_token": "Password updated"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -87,3 +87,30 @@ class Auth:
             return None
         
         return user
+
+
+    def get_reset_password_token(self, email:str) -> str:
+        """reset
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except:
+            raise ValueError
+        user.reset_token = self._generate_uuid()
+        self._db.update_user(user.id,reset_token=user.reset_token)
+        return user.reset_token
+    
+    def update_password(self, reset_token: str, password: str) -> None:
+        """pwd
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except:
+            user = None            
+
+        if user :
+            raise ValueError
+        
+        user.hashed_password = _hash_password(password)
+        user.reset_token = None
+        user = self._db.update_user(user.id,hashed_password=user.hashed_password,reset_token=user.reset_token) 
